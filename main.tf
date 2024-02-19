36b602fd338e1a403c0da7d92caf2219245c9fd6
mohitgaur1272 <138590767+mohitgaur1272@users.noreply.github.com>
2024-02-19 12:46:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,8 @@
-# Create a new VPC
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16" # Specify the CIDR block for your VPC
-  enable_dns_support = true
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = "my-vpc"
-  }
-}
-
 # Create a new security group within the new VPC
 resource "aws_security_group" "my_security_group" {
   name        = "my-sg"
   description = "My example security group"
-  vpc_id      = aws_vpc.my_vpc.id
+  vpc_id      = "paste_your_vpc_id"
 
   ingress {
     from_port   = 22
@@ -69,46 +58,58 @@ resource "aws_instance" "my_instance1" {
   key_name      = aws_key_pair.my_key_pair.key_name # Reference the key pair created above
   count         = 1
   vpc_security_group_ids = [aws_security_group.my_security_group.id]
-  subnet_id     = aws_subnet.my_subnet.id  # Specify the subnet ID within the new VPC
+  subnet_id     = "paste_your_subnet_id"  # Specify the subnet ID within the new VPC
   user_data = <<-EOF
               #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              systemctl start apache2
-              systemctl enable apache2
+              sudo touch install_apache_docker_jenkins.sh
+              echo '#!/bin/bash
 
-              # Install Docker
-              apt-get install -y docker.io
+             # Update package list
+             sudo apt-get update
 
-              # Install Jenkins 
-              apt install openjdk-11-jdk -y
-              curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-              /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+             # Install Apache2 
+             sudo apt-get install -y apache2
 
-              echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] 
-              \https://pkg.jenkins.io/debian-stable binary/ | sudo tee \ 
-              /etc/apt/sources.list.d/jenkins.list > /dev/null
+             # Start Apache2 service and enable it to start on boot
+             sudo systemctl start apache2
+             sudo systemctl enable apache2
 
-              apt update -y
-              apt install jenkins -y
-              systemctl start jenkins
-              systemctl enable jenkins
-             EOF
+            # Install Docker
+            sudo apt-get install -y docker.io
+
+            # Adjust permissions for Docker socket
+            sudo chmod 777 /var/run/docker.sock
+
+            # Add user to the docker group
+            sudo usermod -aG docker ubuntu
+
+            # Install OpenJDK 11
+            sudo apt update -y
+            sudo apt install openjdk-11-jdk -y
+
+            # Add Jenkins GPG key
+            curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo gpg --dearmor -o /usr/share/keyrings/jenkins-keyring.gpg
+
+            # Add Jenkins repository
+            echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.gpg] \
+               https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+             /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+            # Update package list
+            sudo apt update -y
+
+            # Install Jenkins
+            sudo apt install jenkins -y
+
+            # Start Jenkins service and enable it to start on boot
+            sudo systemctl start jenkins
+            sudo systemctl enable jenkins' >    
+
+            sudo chmod +x install_apache_docker_jenkins.sh 
+            sudo ./install_apache_docker_jenkins.sh 
+            EOF
   tags = {
     Name        = "my-instance1"
     Environment = "production"
-  }
-}
-
-# Create a subnet within the new VPC
-resource "aws_subnet" "my_subnet" {
-  vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.0.0/24" # Specify the CIDR block for your subnet
-  availability_zone        = "your-chosen-availability-zone" # Replace with your chosen availability zone
-
-  map_public_ip_on_launch = true # Set to false if you don't want instances in this subnet to have public IPs
-
-  tags = {
-    Name = "my-subnet"
   }
 }
