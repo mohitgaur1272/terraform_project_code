033370736f8ef82fe9583515fd50edb5ba0211f2
mohitgaur1272 <138590767+mohitgaur1272@users.noreply.github.com>
2024-02-22 11:54:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 resource "aws_security_group" "my_security_group" {
   name        = "my-sg"
   description = "My example security group"
-  vpc_id      = "vpc-0794fde5cab1c1c34"
+  vpc_id      = "vpc-0794fde5cab1c1c34" #paste your vpc id that you want 
 
   ingress {
     from_port   = 22
@@ -40,27 +40,30 @@ resource "aws_security_group" "my_security_group" {
   }
 
   tags = {
-    Name        = "my-security-group"
+    Name        = "my-security-group" #replace your desired name 
     Environment = "production"
   }
 }
 
+
+#first of all you have to do create ssh key in your local computer by this command 
+#cmd = ssh-keygen 
+
 # Create a new key pair
 resource "aws_key_pair" "my_key_pair" {
-  key_name   = "MOHIT_KI_KEY" # Specify the name for your key pair
+  key_name   = "MOHIT_KI_KEY" # Specify the name for your key pair that you want 
   public_key = file("/home/ubuntu/.ssh/id_rsa.pub") # Path to your public key file
 }
 
 # Launch an instance within the new VPC
 resource "aws_instance" "my_instance1" {
   ami                    = "ami-0a7cf821b91bcccbc" # Replace with your desired AMI ID
-  instance_type          = "t2.micro"
+  instance_type          = "t2.micro" # replace with your desired instance type 
   key_name               = aws_key_pair.my_key_pair.key_name # Reference the key pair created above
-  count                  = 1
-  vpc_security_group_ids = [aws_security_group.my_security_group.id]
-  subnet_id              = "subnet-08862ae7dee8aa99b"  # Specify the subnet ID within the new VPC
-  user_data = "${file("user-data-apache.sh")}"
-
+  count                  = 1 #Replace with your desired count number
+  vpc_security_group_ids = [aws_security_group.my_security_group.id] # if you want to add your security groud that already created so replace this name
+  subnet_id              = "subnet-08862ae7dee8aa99b"  # replace with your subnet id 
+  user_data = "${file("user-data-apache.sh")}" this is script that will be run on instnace when instance create 
 
 
 tags = {
