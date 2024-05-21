ac4bf61101dceafd4aac2760f588e717e6927740
mohitgaur1272 <138590767+mohitgaur1272@users.noreply.github.com>
2024-05-21 17:01:04 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    #access_key = "AKIAYNWHOCS7W4YQLP2D"
-    #secret_key = "/4V+2nL7zHJOvhOx4IVPxLexa/TP95l2q8BY/v7oy"
+    #access_key = "your_accsess_id"
+    #secret_key = "your_secret_id"
     region = "ap-south-1"
 }
