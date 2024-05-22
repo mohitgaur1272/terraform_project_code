96686a8f49876c0a9f1bcbf9ceeeb4c33a8e29e8
mohitgaur1272 <138590767+mohitgaur1272@users.noreply.github.com>
2024-05-22 17:58:16 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,25 +1,43 @@
+resource "aws_key_pair" "mykey" {
+     key_name = "mohit_21_may_key"
+     public_key = file("C:/Users/Raj/.ssh/id_rsa.pub") #path of your local system public key 
+}
+
 resource "aws_instance" "my_instance1" {
   ami           = "ami-05e00961530ae1b55" # Replace with your desired AMI ID
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.mykey.key_name     # Replace with your key pair name
+  key_name      = aws_key_pair.mykey.key_name # Replace with your key pair name
   count         = 1
-  security_groups = ["21_may"]
+  security_groups = ["22_may"]
   user_data = "${file("user_data1.sh")}"
   tags = {
-    Name        = "21_may_1"
+    Name        = "prometheus"
     Environment = "production"
   }
+  # Root EBS Volume Configuration
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = 8 # Size in GB
+    delete_on_termination = true
+  }
 }
+
 
 resource "aws_instance" "my_instance2" {
   ami           = "ami-05e00961530ae1b55" # Replace with your desired AMI ID
   instance_type = "t2.micro"
   key_name      = aws_key_pair.mykey.key_name     # Replace with your key pair name
   count         = 1
-  security_groups = ["21_may"]
+  security_groups = ["22_may"]
   user_data = "${file("user_data2.sh")}"
   tags = {
-    Name        = "21_may_2"
+    Name        = "grafana"
     Environment = "production"
+  }
+  # Root EBS Volume Configuration
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = 10 # Size in GB
+    delete_on_termination = true
   }
 }
